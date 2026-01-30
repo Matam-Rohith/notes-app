b5a0363065224b60197abd9fee88a58b13043c35
Matam Rohith <matamrohith12614@gmail.com>
2026-01-30 16:25:45 +0530

    Fixed Render port issue

--- backend/index.js
+++ backend/index.js
@@ -31,9 +31,12 @@ app.delete("/api/notes/:id", (req, res) => {
   res.json({ message: "Deleted successfully" });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 // UPDATE note
 app.put("/api/notes/:id", (req, res) => {
